84effbe0578d0362d51144a4dedc4021e4ac7592
Blessing-AkP <blessing.as262@gmail.com>
2023-10-07 13:48:39 +0100

    added header and footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,100 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import {BsLinkedin, BsYoutube, BsGithub, BsInstagram} from 'react-icons/bs';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <footer className='py-4'>
+      <div  className='container-xl'>
+        <div className='row align-items-center'>
+          <div className='col-5'>
+            <div className='footer-top-date d-flex align-items-center gap-30'>
+              <img src='images/newsletter.png' alt='newsletter' />
+              <h2 className='mb-0 text-white'>
+                Sign Up for Newsletter
+              </h2>
+            </div>
+          </div>
+          <div className='col-7'>
+              <div className="input-group">
+                <input type="text" className="form-control py-1" placeholder="Your Email Address" aria-label="Your Email Address" aria-describedby="basic-addon2" />
+                <span className="input-group-text py-2" id="basic-addon2">Subscribe</span>
+              </div>
+            </div>
+        </div>
+      </div>
+    </footer>
+    <footer className='py-3'>
+      <div className='container-xl'>
+        <div className='row'>
+          <div className='col-4'>
+            <h4 className='text-white mb-4'>Contact</h4>
+            <div className=''>
+              <address className='text-white fs-6'>
+                Hno: 115 Adetola Street<br />Aguda, Surulere, Lagos.<br />
+                pinCode: 13110
+              </address>
+              <a href='tel: +234 8110729847' className='text-white mb-0 mt-3 d-block'>+234 8110729847</a>
+              <a href='mailto: blessdgstore@gmail.com' className='text-white mb-0 mt-2 d-block'>blessdgstore@gmail.com</a> 
+              <div className='social-icons d-flex align-items-center gap-30 mt-4'>
+                <a href='' className='text-white'>
+                  <BsLinkedin className='fs-5'/>
+                </a>
+                <a href='' className='text-white'>
+                  <BsInstagram className='fs-5'/>
+                </a>
+                <a href='' className='text-white'>
+                  <BsGithub className='fs-5'/>
+                </a>
+                <a href='' className='text-white'>
+                  <BsYoutube className='fs-5'/>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='col-3'>
+            <h4 className='text-white mb-4'>Information</h4>
+            <div className='footer-link d-flex flex-column'>
+              <Link className='text-white py-2 mb-1'>Priacy Policy</Link>
+              <Link className='text-white py-2 mb-1'>Shipping Policy</Link>
+              <Link className='text-white py-2 mb-1'>Refund Policy</Link>
+              <Link className='text-white py-2 mb-1'>Terms of Service</Link>
+              <Link className='text-white py-2 mb-1'>Blog</Link>
+            </div>
+          </div>
+          <div className='col-3'>
+            <h4 className='text-white mb-4'>Account</h4>
+            <div className='footer-link d-flex flex-column'>
+              <Link className='text-white py-2 mb-1'>About Us</Link>
+              <Link className='text-white py-2 mb-1'>FAQs</Link>
+              <Link className='text-white py-2 mb-1'>Contact</Link>
+            </div>
+          </div>
+          <div className='col-2'>
+            <h4 className='text-white mb-4'>Quick Links</h4>
+            <div className='footer-link d-flex flex-column'>
+              <Link className='text-white py-2 mb-1'>Laptop</Link>
+              <Link className='text-white py-2 mb-1'>Heaphone</Link>
+              <Link className='text-white py-2 mb-1'>Tablet</Link>
+              <Link className='text-white py-2 mb-1'>Watch</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <footer className='py-4 f3'>
+      <div className='container-xl'>
+        <div className='row'>
+          <div className='col-12'>
+            <p className='text-center text-white mb-0'>
+              &copy {new Date().getFullYear()} Powered By BgStore
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    </>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,63 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className='home-wrapper-1 py-5'>
+        <div className='container-xl'>
+          <div className='row'>
+            <div className='col-6'>
+              <div className='main-banner position-relative'>
+                <img src='images/main-banner-1.jpg' className='img-fluid rounded-3' alt='main-banner'/>
+                <div className='main-banner-content position-absolute'>
+                  <h4>SUPERCHARGED FOR PROS</h4>
+                  <h5>Ipad S13 +Pro.</h5>
+                  <p>From $999.00 or $41.62/mo.<br /> for 24 mo footnote*</p>
+                  <Link className='button'>Buy Now</Link>
+                </div>
+              </div>
+            </div>
+            <div className='col-6'>
+              <div className='d-flex gap-10 flex-wrap justify-content-between align-items-center'>
+                <div className='small-banner position-relative'>
+                  <img src='images/catbanner-01.jpg' className='img-fluid rounded-3' alt='main-banner'/>
+                  <div className='small-banner-content position-absolute'>
+                    <h4>Best Sale</h4>
+                    <h5>Laptop Max.</h5>
+                    <p>From $1699.00 or<br /> $64.78/mo.</p>
+                  </div>
+                </div>
+                <div className='small-banner position-relative'>
+                  <img src='images/catbanner-03.jpg' className='img-fluid rounded-3' alt='main-banner'/>
+                  <div className='small-banner-content position-absolute'>
+                    <h4>New Arival</h4>
+                    <h5>Buy Ipad Air</h5>
+                    <p>From 599 or <br />$49.91/mo.</p>
+                  </div>
+                </div>
+                <div className='small-banner position-relative'>
+                  <img src='images/catbanner-02.jpg' className='img-fluid rounded-3' alt='main-banner'/>
+                  <div className='small-banner-content position-absolute'>
+                    <h4>15% Off</h4>
+                    <h5>Smart Watch 7</h5>
+                    <p>Shope the latest band, <br />styles and colors</p>
+                  </div>
+                </div>
+                <div className='small-banner position-relative'>
+                  <img src='images/catbanner-04.jpg' className='img-fluid rounded-3' alt='main-banner'/>
+                  <div className='small-banner-content position-absolute'>
+                    <h4>Free Engraving</h4>
+                    <h5>Quality Headset</h5>
+                    <p>High-fidelity playback &<br /> ultra-low distortion</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
